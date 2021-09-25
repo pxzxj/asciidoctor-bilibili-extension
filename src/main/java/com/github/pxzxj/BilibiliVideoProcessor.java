6ca9c390df51bcc8c7a06cd710bce67b05c0a6f3
pxzxj <pudge.zxj@gmail.com>
2021-09-25 21:16:01 +0800

    修改iframe大小

--- src/main/java/com/github/pxzxj/BilibiliVideoProcessor.java
+++ src/main/java/com/github/pxzxj/BilibiliVideoProcessor.java
@@ -9,9 +9,9 @@ import java.util.Map;
 @Name("bilibili")
 public class BilibiliVideoProcessor extends BlockMacroProcessor {
 
-    private final String CONTENT = "<div style=\"position: relative; padding: 30%;\">\n" +
-            "  <iframe src=\"//player.bilibili.com/player.html?bvid=target&high_quality=1&page=1\" style=\"border-width: 0; position: absolute; width: 100%; height: 100%; top: 0; left: 0;\" allowfullscreen></iframe>\n" +
-            "</div>";
+    private final String CONTENT = "<div class=\"videoblock\"><div class=\"content\">\n" +
+            "  <iframe width=\"600\" height=\"500\" src=\"//player.bilibili.com/player.html?bvid=target&high_quality=1&page=1\"  border=\"0\" frameborder=\"no\" framespacing=\"0\" scrolling=\"no\" allowfullscreen=\"true\"></iframe>\n" +
+            "</div></div>";
 
     public Object process(StructuralNode parent, String target, Map<String, Object> attributes) {
         return createBlock(parent, "pass", CONTENT.replace("target", target));
