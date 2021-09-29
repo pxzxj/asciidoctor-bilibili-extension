e0bd68d6177d509f251d134d58a41505558a3658
pxzxj <zhangxiongjiang@ponshine.com>
2021-09-29 20:03:51 +0800

    1. 支持使用width和height属性设置播放器的宽和高 2. 补充README.md

--- src/main/java/com/github/pxzxj/BilibiliVideoProcessor.java
+++ src/main/java/com/github/pxzxj/BilibiliVideoProcessor.java
@@ -14,7 +14,23 @@ public class BilibiliVideoProcessor extends BlockMacroProcessor {
             "</div></div>";
 
     public Object process(StructuralNode parent, String target, Map<String, Object> attributes) {
-        return createBlock(parent, "pass", CONTENT.replace("target", target));
+        StringBuilder iframe = new StringBuilder("<div class=\"videoblock\"><div class=\"content\">\n");
+        if(attributes.containsKey("title")) {
+            iframe.append("<div class=\"title\">").append(attributes.get("title")).append("</div>");
+        }
+        Object width = "640";
+        if(attributes.containsKey("width")) {
+            width = attributes.get("width");
+        }
+        Object height = "480";
+        if(attributes.containsKey("height")) {
+            height = attributes.get("height");
+        }
+        iframe.append("<iframe width=\"").append(width).append("\" height=\"").append(height)
+                .append("\" src=\"https://player.bilibili.com/player.html?bvid=").append(target)
+                .append("&high_quality=1&page=1\"  border=\"0\" frameborder=\"no\" framespacing=\"0\" scrolling=\"no\" allowfullscreen=\"true\"></iframe>\n")
+                .append("</div></div>");
+        return createBlock(parent, "pass", iframe.toString());
     }
 
 }
